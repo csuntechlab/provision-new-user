8fd16d91fe39c2b34a8e814e95355bb195e1c996
Matthew Fritz <matthew.fritz@csun.edu>
2017-07-06 14:21:05 -0700

    Refactored the sending of emails into an event handler

--- app/Http/Controllers/ProvisionController.php
+++ app/Http/Controllers/ProvisionController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Google_Service_Gmail;
-use Google_Service_Gmail_Message;
+use App\Events\SendEmailEvent;
 
 class ProvisionController extends Controller
 {
@@ -18,24 +17,13 @@ class ProvisionController extends Controller
       return view('pages.provision.index');
    }
 
-   public function provisionUser(Google_Service_Gmail $service) {
-      // http://www.pipiscrew.com/2015/04/php-send-mail-through-gmail-api/
-      $encodedSubject = base64_encode("Encoded Subject");
-
-      $messageText = "To: Matthew Fritz <matthew.fritz@metalab.csun.edu>\r\n";
-      $messageText .= "From: Matthew Fritz <matthew.fritz@metalab.csun.edu>\r\n";
-      $messageText .= "Subject: =?utf-8?B?{$encodedSubject}?=\r\n";
-      $messageText .= "MIME-Version: 1.0\r\n";
-      $messageText .= "Content-Type: text/html; charset=utf-8\r\n";
-      $messageText .= "Content-Transfer-Encoding: quoted-printable\r\n\r\n";
-      $messageText .= "This is the test message<br />\r\n";
-      $messageText .= "This is another line on the test message<br />\r\n";
-
-      $message = new Google_Service_Gmail_Message();
-      $raw = rtrim(strtr(base64_encode($messageText), '+/', '-_'), '=');
-      $message->setRaw($raw);
-
-      $sent = $service->users_messages->send("matthew.fritz@metalab.csun.edu", $message);
-      dd($sent);
+   public function provisionUser() {
+      // fire the event to send the message
+      event(new SendEmailEvent(
+         "matthew.fritz@metalab.csun.edu",
+         "matthew.fritz@metalab.csun.edu",
+         "Using Event Handler",
+         "Line one<br />Line two"
+      ));
    }
 }
---
+++ app/Listeners/EmailEventListener.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SendEmailEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Google_Service_Gmail;
+use Google_Service_Gmail_Message;
+
+class EmailEventListener
+{
+    protected $service;
+
+    /**
+     * Create the event listener.
+     *
+     * @param Google_Service_Gmail $service Gmail API instance
+     */
+    public function __construct(Google_Service_Gmail $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  SendEmailEvent  $event
+     * @return void
+     */
+    public function handle(SendEmailEvent $event)
+    {
+       // http://www.pipiscrew.com/2015/04/php-send-mail-through-gmail-api/
+       $encodedSubject = base64_encode($event->subject);
+
+       $messageText = "To: {$event->to}\r\n";
+       $messageText .= "From: {$event->from}\r\n";
+       $messageText .= "Subject: =?utf-8?B?{$encodedSubject}?=\r\n";
+       $messageText .= "MIME-Version: 1.0\r\n";
+       $messageText .= "Content-Type: text/html; charset=utf-8\r\n";
+       $messageText .= "Content-Transfer-Encoding: quoted-printable\r\n\r\n";
+       $messageText .= "{$event->body}\r\n";
+
+       $message = new Google_Service_Gmail_Message();
+       $raw = rtrim(strtr(base64_encode($messageText), '+/', '-_'), '=');
+       $message->setRaw($raw);
+
+       // send out the email
+       $this->service->users_messages->send($event->to, $message);
+    }
+}
